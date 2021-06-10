596d32b485d29f2328b51d599e206cc18b953dd9
Gregory Mc-Bit <grigory.2004@yandex.ru>
2021-06-10 13:42:01 +0300

    refactor: поправить стили, удалить ненужные импорты и классы

--- scripts/script.js
+++ scripts/script.js
@@ -1,49 +1,49 @@
-let likes = document.querySelectorAll('.grid-like')
+let likes = document.querySelectorAll('.grid-places__like')
 
 for (let i=0; i<=likes.length - 1; i+=1) {
     let like = likes[i]
     like.addEventListener('click', function () {
-        like.classList.toggle('grid-like_active')
+        like.classList.toggle('grid-places__like_active')
     })
 }
 
 // popup_functions
-function open_popup() {
-    let popup = document.querySelector('.popup')
+function openPopup() {
     popup.classList.add('popup_opened')
 
     // filling forms with current values
-    form_name.value = current_form_name.textContent
-    form_profession.value = current_form_profession.textContent
+    formName.value = currentFormName.textContent
+    formProfession.value = currentFormProfession.textContent
 }
 
-function close_popup() {
-    let popup = document.querySelector('.popup')
+function closePopup() {
     popup.classList.remove('popup_opened')
 }
 
-function form_saving(evt) {
+function formSaving(evt) {
     evt.preventDefault()
 
-    current_form_name.textContent = form_name.value
-    current_form_profession.textContent = form_profession.value
+    currentFormName.textContent = formName.value
+    currentFormProfession.textContent = formProfession.value
 
-    close_popup()
+    closePopup()
 }
 
+let popup = document.querySelector('.popup')
+
 // open popup
-let popup_open_btn = document.querySelector('.profile__btn-image')
-popup_open_btn.addEventListener('click', open_popup)
+let popupOpenBtn = document.querySelector('.profile__btn-image')
+popupOpenBtn.addEventListener('click', openPopup)
 
 // close popup
-let popup_close_btn = document.querySelector('.popup__button-close')
-popup_close_btn.addEventListener('click', close_popup)
+let popupCloseBtn = document.querySelector('.popup__button-close')
+popupCloseBtn.addEventListener('click', closePopup)
 
 // form inputs
-let form_name = document.querySelector('#name')
-let form_profession = document.querySelector('#profession')
-let current_form_name = document.querySelector('.profile__name')
-let current_form_profession = document.querySelector('.profile__information')
+let formName = document.querySelector('#name')
+let formProfession = document.querySelector('#profession')
+let currentFormName = document.querySelector('.profile__name')
+let currentFormProfession = document.querySelector('.profile__information')
 
-let form_save_btn = document.querySelector('.popup__button-save')
-form_save_btn.addEventListener('click', form_saving)
+let formSaveBtn = document.querySelector('.popup__button-save')
+formSaveBtn.addEventListener('click', formSaving)
