4b20598b04e2d0220d8fe7a548ed946a2605c379
Gregory Mc-Bit <grigory.2004@yandex.ru>
2021-08-07 20:51:51 +0300

    fix

--- scripts/script.js
+++ scripts/script.js
@@ -1,30 +1,30 @@
 // initial adding
 const initialCards = [
-    {
-      name: 'Архыз',
-      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg'
-    },
-    {
-      name: 'Челябинская область',
-      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg'
-    },
-    {
-      name: 'Иваново',
-      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg'
-    },
-    {
-      name: 'Камчатка',
-      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg'
-    },
-    {
-      name: 'Холмогорский район',
-      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg'
-    },
-    {
-      name: 'Байкал',
-      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg'
-    }
-  ];
+  {
+    name: 'Архыз',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg'
+  },
+  {
+    name: 'Челябинская область',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg'
+  },
+  {
+    name: 'Иваново',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg'
+  },
+  {
+    name: 'Камчатка',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg'
+  },
+  {
+    name: 'Холмогорский район',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg'
+  },
+  {
+    name: 'Байкал',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg'
+  }
+];
 
 const photoZoneTemplate = document.querySelector('#grid-template').content
 const photos = document.querySelector('.grid-places')
@@ -34,105 +34,105 @@ const profilePopup = document.querySelector('#profile')
 const openProfileBtn = document.querySelector('.profile__btn-image')
 const openAddPhotoBtn = document.querySelector('.profile__add-btn')
 
+const formName = document.querySelector('#name')
+const formProfession = document.querySelector('#profession')
+const currentFormName = document.querySelector('.profile__name')
+const currentFormProfession = document.querySelector('.profile__information')
+const profileForm = document.forms.profileForm
+const closeProfileBtn = document.querySelector('#profile-close')
+
+const addForm = document.forms.addPlace
+const closeBtn = addPlacePopup.querySelector('#photo-close')
+
+addForm.addEventListener('submit', function(evt) {
+evt.preventDefault()
+
+const title = addPlacePopup.querySelector('#title').value
+const link = addPlacePopup.querySelector('#link').value
+
+const card = createCard(title, link)
+photos.prepend(card)
+closePopup(addPlacePopup)
+document.forms.addPlace.reset()
+})
+
+closeBtn.addEventListener('click', function() {
+closePopup(addPlacePopup)
+
+document.forms.addPlace.reset()
+})
+
 openProfileBtn.addEventListener('click', editProfile)
 openAddPhotoBtn.addEventListener('click', addPlace)
 
 initialCards.forEach(item => {
-  const newCard = createCard(item.name, item.link)
-  photos.append(newCard)
+const newCard = createCard(item.name, item.link)
+photos.append(newCard)
 })
 
 // function to add new card
 function createCard(name, link) {
-  const photoZone = photoZoneTemplate.querySelector('.grid-places__item').cloneNode(true);
+const photoZone = photoZoneTemplate.querySelector('.grid-places__item').cloneNode(true);
 
-  const like = photoZone.querySelector('.grid-places__like')
-  like.addEventListener('click', function () {like.classList.toggle('grid-places__like_active')})
+const like = photoZone.querySelector('.grid-places__like')
+like.addEventListener('click', function () {like.classList.toggle('grid-places__like_active')})
 
-  const deleteBtn = photoZone.querySelector('.grid-places__delete-icon')
-  deleteBtn.addEventListener('click', function(evt) {
-    const parent = evt.target.parentElement
-    parent.remove()
-  })
+const deleteBtn = photoZone.querySelector('.grid-places__delete-icon')
+deleteBtn.addEventListener('click', function(evt) {
+  const parent = evt.target.parentElement
+  parent.remove()
+})
 
-  photoZone.querySelector('.grid-places__image').addEventListener('click', showPhoto)
+photoZone.querySelector('.grid-places__image').addEventListener('click', showPhoto)
 
-  photoZone.querySelector('.grid-places__image').src = link
-  photoZone.querySelector('.grid-places__image').alt = name
-  photoZone.querySelector('.grid-places__title').textContent = name
+photoZone.querySelector('.grid-places__image').src = link
+photoZone.querySelector('.grid-places__image').alt = name
+photoZone.querySelector('.grid-places__title').textContent = name
 
-  return photoZone
+return photoZone
 }
 
 function showPhoto(evt) {
-  const photo = evt.target.src
-  const photoZone = photoPopup.querySelector('.popup__photo')
-  photoZone.src = photo
+const photo = evt.target.src
+const photoZone = photoPopup.querySelector('.popup__photo')
+photoZone.src = photo
 
-  const closeBtn = document.querySelector('#close')
-  closeBtn.addEventListener('click', function() {
-    closePopup(photoPopup)
-  })
+const closeBtn = document.querySelector('#close')
+closeBtn.addEventListener('click', function() {
+  closePopup(photoPopup)
+})
 
-  openPopup(photoPopup)
+openPopup(photoPopup)
 }
 
 function editProfile() {
-  const formName = document.querySelector('#name')
-  const formProfession = document.querySelector('#profession')
-  const currentFormName = document.querySelector('.profile__name')
-  const currentFormProfession = document.querySelector('.profile__information')
+formName.value = currentFormName.textContent
+formProfession.value = currentFormProfession.textContent
 
-  formName.value = currentFormName.textContent
-  formProfession.value = currentFormProfession.textContent
+profileForm.addEventListener('submit', function(evt) {
+  evt.preventDefault()
 
-  const saveProfileBtn = document.querySelector('#saveProfile')
-  saveProfileBtn.addEventListener('click', function(evt) {
-    evt.preventDefault()
+  currentFormName.textContent = formName.value
+  currentFormProfession.textContent = formProfession.value
 
-    currentFormName.textContent = formName.value
-    currentFormProfession.textContent = formProfession.value
+  closePopup(profilePopup)
+})
 
-    closePopup(profilePopup)
-  })
+closeProfileBtn.addEventListener('click', function() {
+  closePopup(profilePopup)
+})
 
-  const closeProfileBtn = document.querySelector('#profile-close')
-  closeProfileBtn.addEventListener('click', function() {
-    closePopup(profilePopup)
-  })
-
-  openPopup(profilePopup)
+openPopup(profilePopup)
 }
 
 function addPlace() {
-  openPopup(addPlacePopup)
-
-  const saveBtn = addPlacePopup.querySelector('#photo-save')
-  const closeBtn = addPlacePopup.querySelector('#photo-close')
-
-  saveBtn.addEventListener('click', function(evt) {
-    evt.preventDefault()
-
-    const title = addPlacePopup.querySelector('#title').value
-    const link = addPlacePopup.querySelector('#link').value
-
-    const card = createCard(title, link)
-    photos.append(card)
-    closePopup(addPlacePopup)
-    document.forms.addPlace.reset()
-  })
-
-  closeBtn.addEventListener('click', function() {
-    closePopup(addPlacePopup)
-
-    document.forms.addPlace.reset()
-  })
+openPopup(addPlacePopup)
 }
 
 function openPopup(popup) {
-  popup.classList.add('popup_opened')
+popup.classList.add('popup_opened')
 }
 
 function closePopup(popup) {
-  popup.classList.remove('popup_opened')
+popup.classList.remove('popup_opened')
 }
